b79a1840b16c9a3226015158002b0688d9e169e9
Ashwin901 <ashwinprasanna9@gmail.com>
2022-12-20 22:19:07 +0530

    add spec and status details for CR

--- pkg/apis/operators/v1alpha1/types.go
+++ pkg/apis/operators/v1alpha1/types.go
@@ -5,15 +5,27 @@ import (
 )
 
 type SocialBook struct {
-	metav1.TypeMeta
-	metav1.ObjectMeta
+	metav1.TypeMeta   `json:",inline"`
+	metav1.ObjectMeta `json:"metadata,omitempty"`
 
-	Spec SocialBookSpec
+	Spec   SocialBookSpec   `json:"spec,omitempty"`
+	Status SocialBookStatus `json:"status,omitempty"`
 }
 
 type SocialBookSpec struct {
-	Replicas int32
-	UserName string
-	Password string
-	Port     string
+	Replicas int32  `json:"replicas,omitempty"`
+	UserName string `json:"username,omitempty"`
+	Password string `json:"pwd,omitempty"`
+	Port     string `json:"port,omitempty"`
+}
+
+type SocialBookStatus struct {
+	AvailableReplicas int32 `json:"availableReplicas,omitempty"`
+}
+
+type SocialBookList struct {
+	metav1.TypeMeta `json:",inline"`
+	metav1.ListMeta `json:"metadata,omitempty"`
+
+	Items []SocialBook `json:"items,omitempty"`
 }
